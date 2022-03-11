d200b20ff8f5da5e7400433d61a252eec540cc7e
JohniCassere <johnicassere@hotmail.com>
2022-03-11 02:30:45 -0300

    Tentando Chegar La

--- src/components/Bolas/index.js
+++ src/components/Bolas/index.js
@@ -5,32 +5,40 @@ import axios from 'axios';
 
 export const Sorteio = ()=> {
 
-  const [ sorteio, setSorteio ] = useState([]);
+const [ sorteio, setSorteio ] = useState([]);
 
 const getSorteio = async () => {
   await axios.get('/bola').then((response) => {
-    console.log(response.data + " RESPONSE");
-     
+    setSorteio(response.data);
+    console.log(response.data);  
   });
-  
 };
-
 useEffect(() =>{
   getSorteio();
 }, []);
+const msg = 'Gerando ...'
+
 
     return( 
       <Figure>
+        <h1>{`${msg}`}</h1>
       <Figure.Image
         width={200}
         height={350}
         alt="171x180"
        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu8sKkLo4NarWau9bXf5MWvFq74tdKqufDEA&usqp=CAU"
       />
+      
+        
+      
+      
       <Figure.Caption>
         Cada cartela custa 50 moedas.
-      </Figure.Caption> 
-          
+      </Figure.Caption>
+      <Card >
+  
+        </Card> 
+        
     </Figure>
     
     )
